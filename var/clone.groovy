feeb51c96d5d0bd09a6bf172f9ded6e5d4b18d65
Abhinav Bhatnagar <abhi.bhatnagar61@yahoo.com>
2025-10-19 10:45:31 +0530

    Add function to clone Git repository

---
+++ var/clone.groovy
@@ -0,0 +1,8 @@
+def call (String url, String branch){
+
+git branch: 'main', 
+				
+				credentialsId: 'Github',	
+				url: 'https://github.com/Maverick-333/node-project-app.git'
+  echo "clone done"
+}
